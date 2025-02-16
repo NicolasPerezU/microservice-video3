306ca0334fb4a63e098700bc852337819c282e67
NicolasPerezU <u20221204357@usco.edu.co>
2025-02-16 09:54:47 -0500

    eureka

--- microservice-motorcycle/src/main/java/com/nicolas/microservice_motorcycle/MicroserviceMotorcycleApplication.java
+++ microservice-motorcycle/src/main/java/com/nicolas/microservice_motorcycle/MicroserviceMotorcycleApplication.java
@@ -2,8 +2,10 @@ package com.nicolas.microservice_motorcycle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceMotorcycleApplication {
 
 	public static void main(String[] args) {
--- microservice-user/src/main/java/com/nicolas/microservice_user/MicroserviceUserApplication.java
+++ microservice-user/src/main/java/com/nicolas/microservice_user/MicroserviceUserApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class MicroserviceUserApplication {
 
 	public static void main(String[] args) {
